eca69c03894934d9bde651a72e5a27fb1950aea4
malak_bendalla <malak.bendalla22@gmail.com>
2024-03-06 13:34:33 +0100

    school service + app.yml

---
+++ school/src/main/java/com/malak/school/SchoolController.java
@@ -0,0 +1,38 @@
+package com.malak.school;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/schools")
+@RequiredArgsConstructor
+public class SchoolController {
+
+    private final SchoolService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(
+            @RequestBody School school
+    ) {
+        service.saveSchool(school);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<School>> findAllSchools() {
+        return ResponseEntity.ok(service.findAllSchools());
+    }
+
+    @GetMapping("/with-students/{school-id}")//must be the same in student client
+    //public ResponseEntity<FullSchoolResponse> findAllSchools(
+    public ResponseEntity<FullSchoolResponse> findAllSchools(
+
+            @PathVariable("school-id") Integer schoolId
+    ) {
+        return ResponseEntity.ok(service.findSchoolsWithStudents(schoolId));
+    }
+}
